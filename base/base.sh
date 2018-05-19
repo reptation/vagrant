84a9ed88cee83ef55f67578bb54e08992d19c4ec
root <root@base>
2018-05-19 21:27:54 +0000

    git config

--- base/base.sh
+++ base/base.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-apt-get update
-apt-get install -y htop curl
+apt-get update 
+apt-get install -y htop unzip
 cd /home/vagrant
 git clone https://github.com/reptation/scripts.git
 cd scripts
 cp .bash_aliases .vimrc ../
 cd ../
 ln -s /vagrant/ vagrant
-chown -R vagrant:vagrant ./*
+git config --global user.email "david@musicalquilt.com"
+git config --global user.name "reptation"
+cp ~/.gitconfig /home/vagrant
 
+
+chown -R vagrant:vagrant ./*
 
