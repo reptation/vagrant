91457b41ed128849ec363ee0a05b62561a088752
reptation <david@musicalquilt.com>
2018-05-21 20:52:58 -0500

    install hvac

--- vault/vault.sh
+++ vault/vault.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-apt-get install jq
+apt install -y jq python3-pip
+ln -s /usr/bin/pip3 /usr/bin/pip
+ln -s /usr/bin/python3 /usr/bin/python
+pip install hvac
 cd /home/vagrant
 wget https://releases.hashicorp.com/vault/0.10.1/vault_0.10.1_linux_amd64.zip
 unzip vault_0.10.1_linux_amd64.zip
