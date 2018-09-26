913209eab16056e1658205365ef29d9b28af311a
reptation <david@musicalquilt.com>
2018-09-25 21:32:54 -0500

    copy scfripts to vagrant ~. make ansible and vagrant use 8080

--- jenkins-master/jenkins-master-install.sh
+++ jenkins-master/jenkins-master-install.sh
@@ -5,9 +5,13 @@ cd ~/
 git clone https://github.com/reptation/scripts.git
 git clone  https://github.com/reptation/vagrant-files.git
 
+#Ansible finds files here
+sudo cp cp -r scripts /home/vagrant/
+sudo chown -R vagrant:vagrant /home/vagrant
+
 #software updates utility packages 
 sudo bash vagrant-files/base/base.sh
 
 #install ansible
-sudo bash  scripts/infrastructure/ansible/ansible-install.sh
+sudo bash scripts/infrastructure/ansible/ansible-install.sh
 
